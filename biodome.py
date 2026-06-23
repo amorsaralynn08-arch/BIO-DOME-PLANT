6d09ded28de926dc6e815defccac4678155e98a9
amor <amorsaralynn@gmail.com>
2026-06-23 12:45:42 +0300

    Connect plant selection UI to Plant classes

--- biodome.py
+++ biodome.py
@@ -15,4 +15,49 @@ class BioDome:
 
               )
         self.title_label.pack(pady = 10)
+        self.stats_label = tk.Label(
+            self.root , 
+            text = "No plant has been selected!",
+            foreground = 556B2F,
+        )
+        self.stats_label.pack(pady=10)
+        tk.Button(
+            self.root,
+            text = "Orchid",
+            command=self.choose_Orchid,
+        ).pack(pady=10)
+        tk.Button(
+            self.root,
+            text = "Strawberry",
+            command=self.choose_Strawberry,
+        ).pack(pady=10)
+        tk.Button(
+            self.root,
+            text = "Bonsai",
+            command=self.choose_Bonsai,
+        ).pack(pady=10)
+        tk.Button(
+            self.root,
+            text = "JadeVine",
+            command=self.choose_JadeVine,
+        ).pack(pady=10)
+
+    def choose_Orchid(self):
+        self.current_plant = Orchid("Royal Orchid")
+        self.update_stats()
+
+    def choose_Strawberry(self):
+        self.current_plant = Strawberry("Strawberry")
+        self.update_stats()
+
+    def choose_JadeVine(self):
+        self.current_plant = JadeVine("JadeVine")
+        self.update_stats()
+    
+    def choose_Bonsai(self):
+        self.current_plant = Bonsai("Bonsai")
+        self.update_stats()
+
+
+
 
