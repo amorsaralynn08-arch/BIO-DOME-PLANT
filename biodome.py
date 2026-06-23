fc10323c0ad15e571d780318e546b242423d0fb5
amor <amorsaralynn@gmail.com>
2026-06-23 12:24:14 +0300

    Create base Tkinter layout for plant selection UI

--- biodome.py
+++ biodome.py
@@ -1,10 +1,18 @@
 import tkinter as tk
+from plant import (Orchid , Strawberry , Bonsai , JadeVine   )
 
-root = tk.Tk()
-root.title("Plant Window")
-root.geometry("400x400")
+class BioDome:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Bio Dome Plant")
+        self.root.geometry("500x500")
+        self.current_plant = None
+        self.title_label = tk.label(
+            self.root ,
+            text = "Choose your plant",
+            font=("Arial" , 18),
+            background = "sky-blue"
 
+              )
+        self.title_label.pack(pady = 10)
 
-root.mainloop()
-
-       
