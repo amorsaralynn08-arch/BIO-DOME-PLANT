57f503ee52d3c209a5ca303f4352519fd761bad7
amor <amorsaralynn@gmail.com>
2026-06-23 09:28:31 +0300

    add plant health system

--- plant.py
+++ plant.py
@@ -10,4 +10,28 @@ class Plant(ABC):
  @abstractmethod
  def grow(self):
       pass
+ def is_alive(self):
+      return self.health > 0
+        
+ def take_damage(self , amount):
+      self.health = max(0, self.health - amount)
  
+ def status_checker(self):
+    if self.health <= 0 :
+     return "dead plant"
+    elif self.health < 30:
+     return "Critical"
+    elif self.health < 50:
+      return "unstable"
+    elif self.health < 70 :
+      return "stable"
+    else :
+      return "healthy"
+        
+ def reset(self):
+    self.health = 100
+    self.level = 1
+    self.coins = 0
+
+
+
